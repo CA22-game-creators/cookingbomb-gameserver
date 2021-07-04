1e30a17c89878624e44ce463773bad29e179e41d
唐木 稜生 <s14044@ca-20013110.cyberagent.co.jp>
2021-07-05 00:31:15 +0900

    delete: domain

--- cluster-game-server/domain/model/account/entity.go
+++ cluster-game-server/domain/model/account/entity.go
@@ -1,27 +1,12 @@
 package domain
 
 import (
-	"errors"
-
 	"github.com/oklog/ulid/v2"
 )
 
 type Account struct {
 	ID   ID
 	Name Name
-}
-
-func New(id ID, name Name) (Account, error) {
-	if (id == ID{}) {
-		return Account{}, errors.New("user id is nil")
-	}
-	if name == "" {
-		return Account{}, errors.New("user name is nil")
-	}
-	return Account{
-		ID:   id,
-		Name: name,
-	}, nil
 }
 
 func FromRepository(id, name string) Account {
--- cluster-game-server/domain/model/account/id.go
+++ cluster-game-server/domain/model/account/id.go
@@ -2,16 +2,6 @@ package domain
 
 import (
 	"github.com/oklog/ulid/v2"
-
-	"github.com/CA22-game-creators/cookingbomb-gameserver/cluster-game-server/errors"
 )
 
 type ID ulid.ULID
-
-func NewID(v ulid.ULID) (ID, error) {
-	if (v == ulid.ULID{}) {
-		return ID{}, errors.InvalidOperation()
-	}
-
-	return ID(v), nil
-}
