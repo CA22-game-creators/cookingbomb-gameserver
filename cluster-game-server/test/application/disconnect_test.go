e93a444cf0526444964de5c7003a377ebd84f07a
moritaoy <moritaoy@outlook.jp>
2021-07-10 19:13:58 +0900

    WIP: update

---
+++ cluster-game-server/test/application/disconnect_test.go
@@ -0,0 +1,91 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	disconnect "github.com/CA22-game-creators/cookingbomb-gameserver/cluster-game-server/application/disconnect"
+	domain "github.com/CA22-game-creators/cookingbomb-gameserver/cluster-game-server/domain/model/account"
+	"github.com/CA22-game-creators/cookingbomb-gameserver/cluster-game-server/errors"
+	mockDomain "github.com/CA22-game-creators/cookingbomb-gameserver/cluster-game-server/mock/domain/model/account"
+	testdata "github.com/CA22-game-creators/cookingbomb-gameserver/cluster-game-server/test/testdata/token"
+)
+
+type disconnectTestHandler struct {
+	disconnect disconnect.InputPort
+
+	repository *mockDomain.MockRepository
+}
+
+func (h *disconnectTestHandler) setupTest(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	h.repository = mockDomain.NewMockRepository(ctrl)
+	h.disconnect = disconnect.New(h.repository)
+}
+func TestDisconnect(t *testing.T) {
+	t.Parallel()
+	var status domain.StatusEnum
+
+	tests := []struct {
+		title    string
+		before   func(disconnectTestHandler)
+		input    disconnect.InputData
+		expected disconnect.OutputData
+	}{
+		{
+			title: "[正常]接続済みを想定したセッショントークンを処理",
+			before: func(h disconnectTestHandler) {
+				status = domain.CONNECTED
+				h.repository.EXPECT().Disconnect(testdata.SessionToken.Valid).Do(func(_ string) interface{} {
+					status = domain.DISCONNECTED_BY_CLIENT
+					return nil
+				})
+				h.repository.EXPECT().GetSessionStatus(testdata.SessionToken.Valid).DoAndReturn(func(_ string) interface{} {
+					return status
+				}).AnyTimes()
+			},
+			input:    disconnect.InputData{SessionToken: testdata.SessionToken.Valid},
+			expected: disconnect.OutputData{Status: domain.DISCONNECTED_BY_CLIENT},
+		},
+		{
+			title: "[異常]未接続を想定したセッショントークンを処理",
+			before: func(h disconnectTestHandler) {
+				status = domain.UNSPECIFIED
+				h.repository.EXPECT().GetSessionStatus(testdata.SessionToken.Valid).DoAndReturn(func(_ string) interface{} {
+					return status
+				}).AnyTimes()
+			},
+			input:    disconnect.InputData{SessionToken: testdata.SessionToken.Valid},
+			expected: disconnect.OutputData{Err: errors.SessionNotActive()},
+		},
+		{
+			title: "[異常]切断済みを想定したセッショントークンを処理",
+			before: func(h disconnectTestHandler) {
+				status = domain.DISCONNECTED_BY_CLIENT
+				h.repository.EXPECT().GetSessionStatus(testdata.SessionToken.Valid).DoAndReturn(func(_ string) interface{} {
+					return status
+				}).AnyTimes()
+			},
+			input:    disconnect.InputData{SessionToken: testdata.SessionToken.Valid},
+			expected: disconnect.OutputData{Err: errors.SessionNotActive()},
+		},
+	}
+
+	for _, td := range tests {
+		td := td
+
+		t.Run("application/disconnect:"+td.title, func(t *testing.T) {
+			var tester disconnectTestHandler
+			tester.setupTest(t)
+			if td.before != nil {
+				td.before(tester)
+			}
+
+			actual := tester.disconnect.Handle(td.input)
+			assert.Equal(t, td.expected, actual)
+		})
+	}
+}
