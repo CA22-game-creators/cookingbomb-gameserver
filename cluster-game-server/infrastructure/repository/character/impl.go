48554bf8ff3cb20b4f3941e14ab591e4cf8903e1
moritaoy <moritaoy@outlook.jp>
2021-07-18 19:53:48 +0900

    fixed: character配列をポインタではなく実値で持つ

--- cluster-game-server/infrastructure/repository/character/impl.go
+++ cluster-game-server/infrastructure/repository/character/impl.go
@@ -7,37 +7,37 @@ import (
 )
 
 type impl struct {
-	characters *[]domain.Character
+	characters []domain.Character
 	mu         *sync.Mutex
 }
 
 func New() domain.Repository {
 	slice := []domain.Character{}
 	return &impl{
-		characters: &slice,
+		characters: slice,
 		mu:         &sync.Mutex{},
 	}
 }
 
 func (i *impl) Add(c domain.Character) int {
 	i.mu.Lock()
-	*i.characters = append(*i.characters, c)
-	index := len(*i.characters) - 1
+	i.characters = append(i.characters, c)
+	index := len(i.characters) - 1
 	i.mu.Unlock()
 	return index
 }
 
 func (i *impl) GetAll() *[]domain.Character {
 	i.mu.Lock()
-	len := len(*i.characters)
+	len := len(i.characters)
 	sl := make([]domain.Character, len)
-	copy(sl, *i.characters)
+	copy(sl, i.characters)
 	i.mu.Unlock()
 	return &sl
 }
 
 func (i *impl) Update(c domain.Character, index int) {
 	i.mu.Lock()
-	(*i.characters)[index] = c
+	(i.characters)[index] = c
 	i.mu.Unlock()
 }
